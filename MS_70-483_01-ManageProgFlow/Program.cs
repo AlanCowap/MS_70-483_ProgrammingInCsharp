06f47896cbb1a9dd4f670c96cc17869cfb52db60
Alan Cowap <spam@alancowap.com>
2016-11-26 20:28:11 +0000

    Add Project for Objective 01

---
+++ MS_70-483_01-ManageProgFlow/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_70_483_01_ManageProgFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MS 70-483 #2 Manage Program Flow");
+        }
+    }
+}
