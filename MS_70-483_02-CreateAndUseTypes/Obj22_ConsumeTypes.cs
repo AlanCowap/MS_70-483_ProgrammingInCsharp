15600d79a2bbe1c83b164ccd59dbc14eada42b1f
Alan Cowap <spam@alancowap.com>
2016-12-16 22:12:37 +0000

    Add code for Tye conversion - implicit & explicit

--- MS_70-483_02-CreateAndUseTypes/Obj22_ConsumeTypes.cs
+++ MS_70-483_02-CreateAndUseTypes/Obj22_ConsumeTypes.cs
@@ -14,9 +14,9 @@ namespace MS_70_483_02_CreateAndUseTypes
             Console.WriteLine(title);
 
             Obj22_ConsumeTypes demo = new Obj22_ConsumeTypes();
-            demo.Obj22_BoxUnbox(title + ": Boxing and Unboxing"); // p107
+            demo.Obj22_BoxUnbox(title + ": Boxing and Unboxing");   //p107
             demo.Obj22_TypeConversion(title + ": Type Conversion"); //p108
-            //demo.Obj22_DynamicTypes(title + ": Dynamic Types"); //p112
+            //demo.Obj22_DynamicTypes(title + ": Dynamic Types");     //p112
         }
 
         private void Obj22_BoxUnbox(String title)
@@ -47,7 +47,38 @@ namespace MS_70_483_02_CreateAndUseTypes
         private void Obj22_TypeConversion(String title)
         {
             Console.WriteLine(title);
+            //C# is (mostly) statically typed i.e. once a type always a type (mostly, C# does have a growth mindset)
+            //Type conversions can be
+            // 1. Implicit conversions
+            // 2. Explicit conversions
+            // 3. User-defined conversions
+            // 4. Conversion with a helper class
 
+            // 1. Implicit conversions
+            //Compiler does these because it knows the conversion is valid e.g. widening conversion
+            int num = 21;
+            double dub = num;
+            //num = dub; //compiler complains
+
+            // 2. Explicit conversions (aka "casting")
+            num = (int) dub; //Compiler is happy, but be VERY CAREFUL because you may have introduced a logical error
+            dub = Math.PI;
+            num = (int) dub; //Again the compiler is happy, but are you?
+            Console.WriteLine("You can't have your PI " + dub + " and cast it " + num);
+            //So what's the big deal you might say
+            dub *= 1000000;
+            num *= 1000000;
+            Console.WriteLine("You can't have your PI " + dub + " and cast it " + num);
+            Console.WriteLine("That explicit cast just cost you " +  (int) (dub - num)  + " and change :/");
+            // Aside: You should use the "decimal" datatype for monetary calculations
+
+
+            // 3. User-defined conversions
+            //You can also add support to your own classes for implicit and explicit conversions (growth mindset!)
+
+
+
+            // 4. Conversion with a helper class
 
 
             Console.WriteLine("\n");
